e9d9ae70518ff62928f777345688d0bb9da33416
BELHAJ Manel <belhaj343@gmail.com>
2022-10-30 14:10:55 +0100

    added components import & logic to the app component

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,79 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import Face from './Face.svelte';
+  import Container from './Container.svelte';
+  import Header from './Header.svelte';
+  import Buttons from './Buttons.svelte';
+  import story from './story';
+  
+  let showHeader = false;
+  let happyScore = 0;
+  let storyIndex = 0;
+$: question = story[storyIndex];
+  $: smileySays = question.end ? finalMessage() : question.smileySays;
+  $: buttons = question.buttons;
+
+$: if (happyScore > 0 && storyIndex === 3) showHeader = true;
+  function clickHandler(e) {
+  if (e.detail.value === 'reset') {
+    storyIndex = 0;
+    happyScore = 0;
+    showHeader = false;
+  } else {
+        storyIndex += 1;
+        happyScore += e.detail.value;
+  }
+  }
+function finalMessage() {
+  if (happyScore > 0) {
+    return question.end.nice;
+  } else if (happyScore < 0) {
+    return question.end.mean;
+  } else {
+    return question.end.neutral;
+  }
+}
+  let name = '';
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
+{#if showHeader}
+  <Header />
+{/if}
+<Container>
+  <input type="text" bind:value={name}>
+  <h1>{name}, {smileySays}</h1>
+  <Face {happyScore} size={storyIndex+1} />
+  <Buttons {buttons} on:click={clickHandler} />
+</Container>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
+  h1 {
+  text-align: center;
+  background: #ffae57;
+  font-size: 1.5em;
+      padding: 0.3em .6em;
+    color: white;
+  border-radius: 50px;
+}
+input {
+  margin: 1em;
+  width: 250px;
+  font-family: 'Nunito', sans-serif;
+  border: 0;
+  outline: 0;
+  background: transparent;
+  border-bottom: 1px solid #ffd580;
+  text-align: center;
+  font-size: 2em;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+  :global(*) {
+  box-sizing: border-box;
+}
+:global(body, html) {
+  margin: 0;
+  height: 100vh;
+  overflow: hidden;
+}
 </style>
